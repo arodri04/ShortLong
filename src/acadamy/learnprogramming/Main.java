f69e7e716645cfbd6e6fe19544f1adbbd779482d
Sam Rodriguez <augustine.rodriguez55@gmail.com>
2020-07-07 17:53:45 -0700

    Shorts and Longs

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -13,5 +13,31 @@ public class Main {
 
         System.out.println("The Min is: " + myMin);
         System.out.println("The Max is: " + myMax);
+        System.out.println("Busted Max Value = " + (myMax+1));
+
+        int myMaxIntTest = 2147483647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Min Byte = " + myMinByteValue);
+
+        System.out.println("Max Byte = " + myMaxByteValue);
+
+        short myMinshortValue = Short.MIN_VALUE;
+        short myMaxshortValue = Short.MAX_VALUE;
+
+        System.out.println("Min short = " + myMinshortValue);
+
+        System.out.println("Max short = " + myMaxshortValue);
+
+        long myLongValue = 100L;
+        long myMinlongValue = Long.MIN_VALUE;
+        long myMaxlongValue = Long.MAX_VALUE;
+
+        System.out.println("Min long = " + myMinlongValue);
+
+        System.out.println("Max long = " + myMaxlongValue);
+
     }
 }
