4079c91be193ffa6645a1a70d88f4092f91e036a
Sam Rodriguez <augustine.rodriguez55@gmail.com>
2020-07-07 17:40:08 -0700

    MinMax

--- src/acadamy/learnprogramming/Main.java
+++ src/acadamy/learnprogramming/Main.java
@@ -1,8 +1,17 @@
 package acadamy.learnprogramming;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int myValue = 10000;
+
+        int myMax = Integer.MAX_VALUE;
+        int myMin = Integer.MIN_VALUE;
+
+        System.out.println("The Min is: " + myMin);
+        System.out.println("The Max is: " + myMax);
     }
 }
